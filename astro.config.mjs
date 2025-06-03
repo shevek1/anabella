142bfa0c6711e834e9d4743b4fa35bc4d9daf4b8
fabri <nohaymasintentos@gmail.com>
2025-06-03 19:47:41 -0300

    upload page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,13 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://shevek1.github.io',
+  base: 'anabella',
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react()],
+  
 });
+
