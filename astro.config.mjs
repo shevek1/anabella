ae573175a8c261566ae319eb46625e2b2f72b849
fabri <nohaymasintentos@gmail.com>
2025-06-04 00:17:14 -0300

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://shevek1.github.io',
   base: '/anabella',
-
-  // vite: {
-  //   plugins: [tailwindcss()]
-  // },
-
-  integrations: [react()],
-
-
+  integrations: [react(), tailwind()],
 });
